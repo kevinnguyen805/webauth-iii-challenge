6321a0431ddcf524b5d8e6dc82eef369b79974c3
admin <admin@admins-MacBook-Pro.local>
2019-11-13 12:19:13 -0800

    usersModel completed

---
+++ users/usersModel.js
@@ -0,0 +1,30 @@
+const db = require('../data/db-config.js')
+
+module.exports = {
+     add,
+     find,
+     findBy,
+     findById
+}
+
+function find(){
+     return db('users')
+     .select('id', 'username', 'password')
+}
+
+//TODO - make sure to include the role information
+function findBy(filter){
+     return db('users')
+     .where('department', '=', filter)
+}
+
+//TODO - remember to add first() when looking for ID so you can take obj out of array
+function findById(id){
+     return db('users')
+     .where('id', '=', id)
+     .first()
+}
+
+function add(newUser){
+     return db('users').insert(newUser).then(([id]) => findById(id))
+}
